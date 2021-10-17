f6bf6e750939cc07bed89d28a4c9f8a05af9a32b
Renato Mello Konflanz <renato.konflanz@unochapeco.edu.br>
2021-10-17 11:01:31 -0300

    Áudios de martelada

--- src/script/MusicController.gd
+++ src/script/MusicController.gd
@@ -2,6 +2,12 @@ extends Node
 
 var dialogoMusic = load("res://audio/dialogoMusic.mp3")
 
+var martelada1 = load("res://audio/martelada 1.wav")
+var martelada2 = load("res://audio/martelada 2.wav")
+var martelada3 = load("res://audio/martelada 3.wav")
+var marteladaErrada = load("res://audio/martelada 4.wav")
+var marteladaIndex = 0
+
 # Called when the node enters the scene tree for the first time.
 func playDialogoMusic(delay = 0):
 	if(delay > 0):
@@ -10,6 +16,19 @@ func playDialogoMusic(delay = 0):
 	$Music.volume_db = -30
 	$Music.stream = dialogoMusic
 	$Music.play()
+
+func playMartelada():
+	var i = marteladaIndex % 3
 	
+	if(i == 0):
+		$Environment.stream = martelada1
+	if(i == 1):
+		$Environment.stream = martelada2
+	if(i == 2):
+		$Environment.stream = martelada3
+
+	$Environment.play()
+	marteladaIndex += 1
+
 func pause():
 	$Music.pause()
--- src/script/alvo.gd
+++ src/script/alvo.gd
@@ -12,6 +12,7 @@ func _on_Alvo_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		# se destroi aqui e dps aumenta os pontos da arma
 		spawnFogo()
+		MusicController.playMartelada()
 		givePointsToParent(1)
 		queue_free()
 		get_parent().remove_child(self)
