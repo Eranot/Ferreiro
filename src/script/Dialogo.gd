85eb3575c249206c3c86973c4f70da585952a5a5
Renato Mello Konflanz <renato.konflanz@unochapeco.edu.br>
2021-10-17 02:32:28 -0300

    Sistema de diálogo semi-pronto

--- src/script/Dialogo.gd
+++ src/script/Dialogo.gd
@@ -1,37 +1,34 @@
 extends Node2D
 var index = 0;
 
+export(PackedScene) var nextScene
+
+var backGroundOda = load('res://src/sprite/dialogo/background_oda.png')
+var backGroundOutro = load('res://src/sprite/dialogo/background_outro.png')
+
+var character = {
+	'name': 'Exemplo',
+	'img': 'res://src/sprite/char/exemplo.png'
+}
+
+var ODA = 1
+var OUTRO = 2
+
 var dialogos = [
 	{
-		'text': "Oi meus confrades, teste aqui", 
-		'character': {
-			'name': 'Personagem um',
-			'img': 'res://src/sprite/char/renato.png'
-		}
-	},
-	{
-		'text': "Oi, teste", 
-		'character': {
-			'name': 'Renato',
-			'img': 'res://src/sprite/char/renato.png'
-		}
-	},
-	{
-		'text': "Beibe beibe do biruleibe beibe kkkkkkkkkkkkkkkkkk teste com texto grande", 
-		'character': {
-			'name': 'Personagem um',
-			'img': 'res://src/sprite/char/renato.png'
-		}
+		'text': "Teste", 
+		'character': ODA
 	}
 ]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func start():
 	$TextInterfaceEngine.set_color(Color(255, 255, 255))
 	$TextInterfaceEngine.connect("buff_end", self, "onBuffEnd")
+	$Nome.text = character['name']
+	$Char.texture = load(character['img'])
 	
 	_onNext()
-		
+	
 func onBuffEnd():
 
 	if(index == len(dialogos)):
@@ -45,14 +42,24 @@ func onBuffEnd():
 	
 func _onNext():
 	$TextInterfaceEngine.buff_clear()
-	$TextInterfaceEngine.buff_text(dialogos[index]['text'], 0.05)
+	
+	if(dialogos[index].has('startDelay')):
+		$TextInterfaceEngine.buff_silence(dialogos[index]['startDelay'])
+	
+	$TextInterfaceEngine.buff_text(dialogos[index]['text'], 0.03)
 	$TextInterfaceEngine.set_state($TextInterfaceEngine.STATE_OUTPUT)
-	$Nome.text = dialogos[index]['character']['name']
 	$Done.hide()
 	$Next.hide()
+	
+	if(dialogos[index]['character'] == ODA):
+		$Background.texture = backGroundOda
+		$Nome.add_color_override("font_color", Color(198, 198, 198))
+	else:
+		$Background.texture = backGroundOutro
+		$Nome.add_color_override("font_color", Color(255, 255, 255))
+		
 	index += 1
 	
 func _onDone():
-	# Ir para a prox cena
-	pass
+	get_tree().change_scene_to(nextScene)
 
--- src/script/Outside.gd
+++ src/script/Outside.gd
@@ -1,16 +1,25 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	showDialog()
-
-
-func showDialog():
-	#$Dialogo
-	print("cu")
+	$Dialogo.dialogos = [
+		{
+			'text': "Aqui está! E acredito que suas batalhas ficarão muito melhores agora, pois um guerreiro desta parte, com esta força, e empunhando duas armas muito ágeis, você se tornará invencível.", 
+			'character': $Dialogo.ODA,
+			'startDelay': 1
+		},
+		{
+			'text': "Oi, teste aqui mano", 
+			'character': $Dialogo.OUTRO
+		},
+		{
+			'text': "Beibe beibe do biruleibe beibe kkkkkkkkkkkkkkkkkk teste com texto grande", 
+			'character': $Dialogo.ODA
+		}
+	]
+	
+	$Dialogo.character = {
+		'name': 'Renato',
+		'img': 'res://src/sprite/char/exemplo.png'
+	}
+	
+	$Dialogo.start()
